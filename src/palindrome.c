fc458ecc763d2b5d9d03b46a35ce064365e4d3ac
chone12u <antoine.chone@telecomnancy.eu>
2017-05-05 15:46:16 +0200

    Modif via Frama-C

--- src/palindrome.c
+++ src/palindrome.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+
+
+
 int main()
 {
-  int n,reverse=0 ,temp;
+  unsigned int n=0,reverse=0 ,temp=0;
   printf("Enter a number to check if it is a palindrome or not\n ");
-  scanf("%d ",&n);
+  scanf("%u ",&n);
+  //@ assert n >= 0;
   temp = n ;
+//@ loop invariant temp >= 0;
   while ( temp != 0 )
   {
     reverse = reverse * 10;
@@ -12,8 +17,10 @@ int main()
     temp = temp /10;
   }
   if ( n == reverse )
+    //@ assert n == reverse;
     printf("%d is a palindrome number.\n",n);
   else
+    //@ assert n != reverse;
     printf("%d is not a palindrome number.\n ",n);
   return 0;
 }
