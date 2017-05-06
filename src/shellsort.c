fde5d27feb298a27c8ddb8ac59b341cf44d0a4c1
chone12u <antoine.chone@telecomnancy.net>
2017-05-06 16:30:03 +0200

    Travail avec Frama-C

--- src/shellsort.c
+++ src/shellsort.c
@@ -18,18 +18,30 @@ int main ()
   return 0;
 }
 
+//@ requires size>=0;
 void shellsort(int a[], const int size)
 {
-  int i,j,inc,tmp;
+  int i=0,j=0,inc,tmp=0;
   inc = 3;
+  //@ assert inc == 3;
+  //@ loop invariant inc >= 0;
+  //@ loop assigns j,inc,tmp;
+  //@ loop variant inc;
   while (inc > 0)
   {
+    //@ loop invariant i >= 0;
+    //@ loop assigns j,inc,tmp;
+    //@ loop variant size-i;
     for (i=0; i<size; i++)
     {
       j = i;
       tmp = a[i];
+      //@ assert j-inc >= 0;
+      //@ loop assigns j,inc;
+      //@ loop variant j-inc;
       while ((j>=inc) && ( a[j-inc]>tmp ))
       {
+        //@ assert j-inc < size;
         a [j] = a [j-inc] ;
         j = j - inc ;
       }
@@ -38,15 +50,20 @@ void shellsort(int a[], const int size)
     if(inc/2 != 0)
       inc = inc / 2 ;
     else if( inc == 1 )
+    //@ assert inc == 1;
       inc = 0;
     else
+    //@ assert inc == 0;
       inc = 1;
   }
 }
+
+//@ requires size>=0;
 void display( int a[], const int size)
 {
   int i;
+  //@ loop variant size-i;
   for ( i = 0; i < size; i++)
-  printf("%d", a [i]);
+    printf("%d", a [i]);
   printf("\n");
 }
